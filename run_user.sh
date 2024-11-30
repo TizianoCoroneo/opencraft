5f4e49fe597427f64adcb8e2e2d605d65b04a1c3
TizianoCoroneo <tizianocoroneo@me.com>
2024-11-30 19:36:52 +0100

    chore: improved user script

--- run_user.sh
+++ run_user.sh
@@ -1,7 +1,12 @@
-#! /bin/zsh
+#! /bin/sh
 
-UNITY_PATH=/Applications/Unity/Hub/Editor/2022.3.26f1/Unity.app/Contents/MacOS/Unity
+# macos path
+# UNITY_PATH=/Applications/Unity/Hub/Editor/2022.3.26f1/Unity.app/Contents/MacOS/Unity
+# linux path
+UNITY_PATH=~/Unity/Hub/Editor/2022.3.26f1/Editor/Unity
+DB_ADDRESS=192.168.1.1 # influxDB instance address
+DB_PORT=8086 # influxDB HTTP port
 
-telegraf --config client/unity/my-first-unity-project/telegraf.conf & # Telegraf agent
-$UNITY_PATH -projectPath client/unity/my-first-unity-project/ & # Unity client
+telegraf --config telegraf.conf & # Telegraf agent
+$UNITY_PATH -runTests -batchmode -projectPath client/unity/my-first-unity-project/ -testResults ./test_results.xml -testPlatform PlayMode -testFilter GoForward60Seconds & # Unity client
 
