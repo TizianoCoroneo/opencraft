5de80dbe54a9a1f714a6901e0dd2e6b7eda2f4b4
TizianoCoroneo <tizianocoroneo@me.com>
2024-11-30 23:48:04 +0100

    updated scripts

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 set -eu
-signaling-server/webserver_mac & # Signaling server 7981
+
 server/go/opencraft-go/opencraft-go # Game server port: 7979
+
---
+++ run_signaling.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+set -eu
+
+signaling-server/webserver_mac
+
