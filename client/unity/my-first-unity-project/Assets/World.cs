325ba496e5a9ef48ea2e05c60cfdb8013a2950d3
TizianoCoroneo <tizianocoroneo@me.com>
2024-12-01 01:49:40 +0100

    feat: fixed progressive world loading

--- client/unity/my-first-unity-project/Assets/World.cs
+++ client/unity/my-first-unity-project/Assets/World.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
 using System.Security.Cryptography.X509Certificates;
 using Opencraft.NetCode;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
@@ -24,33 +28,58 @@ public class World : MonoBehaviour
         // }
     }
     
-    private List<Pos2> _loadedChunks = new List<Pos2> { new Pos2 { X = 0, Z = 0 } };
+    private Dictionary<Vector2Int, List<GameObject>> _loadedChunksDict = new Dictionary<Vector2Int, List<GameObject>>();
 
     // Update is called once per frame
     void Update()
     {
-        var centerChunkX = (int)(Player.transform.position.x / 16);
-        var centerChunkZ = (int)(Player.transform.position.z / 16);
+        var loadNextNChunks = 1;
+        var chunkSpacingFactor = 16;
         
-        for (var i = -5; i < 5; i++)
+        // Quantized to column multiples
+        var playerX = (int)(Player.transform.position.x / 16) * 16;
+        var playerZ = (int)(Player.transform.position.z / 16) * 16;
+
+        for (var i = -loadNextNChunks; i <= loadNextNChunks; i++)
         {
-            for (var j = -5; j < 5; j++)
+            for (var j = -loadNextNChunks; j <= loadNextNChunks; j++)
             {
-                var pos = new Pos2 { X = centerChunkX + i * 16, Z = centerChunkZ + j * 16 };
-                if (_loadedChunks.Contains(pos))
-                    continue;
-        
-                _loadedChunks.Add(pos);
-                Net.RequestColumn(pos);
-                goto skip; // load at most one chunk per frame
+                var pos = new Vector2Int { x = playerX + i * chunkSpacingFactor, y = playerZ + j * chunkSpacingFactor };
+                if (_loadedChunksDict.ContainsKey(pos)) continue;
+                
+                _loadedChunksDict.Add(pos, new List<GameObject>());
+                Debug.Log($"Added chunk key {pos}");
+                
+                var networkPos = new Pos2 { X = pos.x, Z = pos.y };
+                Net.RequestColumn(networkPos);
+                goto skipLoadOtherChunk; // avoid loading more than one chunk per frame.
             }
         }
         
-        skip: return;
+        skipLoadOtherChunk:
+        Func<Vector2Int, bool> isFarChunk = pos => math.abs(pos.x - playerX) > (loadNextNChunks * 2) * 16
+                                                   || math.abs(pos.y - playerZ) > (loadNextNChunks * 2) * 16;
+
+        var farPairs = _loadedChunksDict.Where(pair => isFarChunk(pair.Key));
+        var farChunkGameObjects = farPairs.SelectMany(pair => pair.Value);
+
+        foreach (var chunk in farChunkGameObjects) {
+            Destroy(chunk);
+        }
+        
+        var farChunkKeys = farPairs.Select(pair => pair.Key);
+
+        foreach (var key in farChunkKeys)
+        {
+            Debug.Log($"Removed chuck at position {key}");
+            _loadedChunksDict.Remove(key);
+        }
     }
 
     public void InstantiateChunk(Vector2Int position, byte[] blockData)
     {
+        Debug.Log($"Instantiating chuck at position {position}");
+        
         for (var i = 0; i < blockData.Length; i++)
         {
             var v = blockData[i];
@@ -60,10 +89,25 @@ public class World : MonoBehaviour
             var z = i % (16 * 16) / 16;
             var y = i / (16 * 16);
             var pos = new Vector3(x + position.x, y, z + position.y);
+
+            GameObject blockToInstantiate = BlockPrefab;
             switch (v)
             {
-                case 1: Instantiate(BlockPrefab, pos, Quaternion.identity); break; 
-                default: Instantiate(GrassPrefab, pos, Quaternion.identity); break;
+                case 2: blockToInstantiate = GrassPrefab; break; 
+                default: break;
+            }
+            
+            var gameObject = Instantiate(blockToInstantiate, pos, Quaternion.identity);
+            
+            List<GameObject> loadedChunkGameObjects;
+            if (_loadedChunksDict.TryGetValue(position, out loadedChunkGameObjects))
+            {
+                loadedChunkGameObjects.Add(gameObject);
+                _loadedChunksDict[position] = loadedChunkGameObjects;
+            }
+            else
+            {
+                _loadedChunksDict.Add(position, new List<GameObject> { gameObject });    
             }
         }
     }
