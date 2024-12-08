4d1877a8109e144016fbc1196fe7e831ae2e8950
TizianoCoroneo <tizianocoroneo@me.com>
2024-12-08 02:40:45 +0100

    chore: added build server script

---
+++ build_server.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+cd server/go/opencraft-go/cmd/opencraft-go/
+go build
+machine=$(uname -s | grep -qi darwin && echo "mac" || echo "linux")
+mv opencraft-go "../../opencraft-go-${machine}"
+
