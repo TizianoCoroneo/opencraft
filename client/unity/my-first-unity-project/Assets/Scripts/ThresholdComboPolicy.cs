96624ce6239cb4a5937771546d0ab901c8cf8c0e
Robert Klink <roberthklink@gmail.com>
2025-01-14 23:28:42 +0100

    feat: ComboPolicy results

--- client/unity/my-first-unity-project/Assets/Scripts/ThresholdComboPolicy.cs
+++ client/unity/my-first-unity-project/Assets/Scripts/ThresholdComboPolicy.cs
@@ -13,9 +13,9 @@ public class ThresholdComboPolicy : Policy
 {
     public override PolicyResult Evaluate(PolicyData data)
     {
-        if (data.stats.RTT > 100 && data.stats.GPU < 40 && data.stats.BatteryWatt < 25)
+        if (data.stats.RTT > 100 && data.stats.GPU < 40 && data.stats.BatteryWatt < 20)
             return PolicyResult.BecomeClient;
-        else if (data.stats.RTT < 85 && data.stats.FPS < 20 || data.stats.GPU > 85 || data.stats.BatteryWatt > 35)
+        else if (data.stats.RTT < 85 && data.stats.FPS < 28 && data.stats.GPU > 60 && data.stats.BatteryWatt > 25)
             return PolicyResult.BecomeThinClient;
         else
             return PolicyResult.None;
