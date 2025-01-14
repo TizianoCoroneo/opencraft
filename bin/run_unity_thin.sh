cfbaf5c6e4e20e53686074d7a4d6e63b68f3b8cf
Robert Klink <roberthklink@gmail.com>
2025-01-14 19:05:11 +0100

    fix: add optimus to unity

--- bin/run_unity_thin.sh
+++ bin/run_unity_thin.sh
@@ -3,4 +3,4 @@
 UNITY_PATH=$(uname -s | grep -qi darwin && \
     echo "/Applications/Unity/Hub/Editor/2022.3.26f1/Unity.app/Contents/MacOS/Unity" || \
     echo "$HOME/Unity/Hub/Editor/2022.3.26f1/Editor/Unity")
-$UNITY_PATH -projectPath ../client/unity/my-first-unity-project/
+__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia $UNITY_PATH -projectPath ../client/unity/my-first-unity-project/
